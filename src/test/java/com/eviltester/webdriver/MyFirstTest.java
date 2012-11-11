233ed7f785ea2ea47ecb7cab645814aaed8101a3
alan <alan@compendiumdev.co.uk>
2012-11-11 10:37:06 +0000

    added basic tutorial files

---
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -0,0 +1,22 @@
+package com.eviltester.webdriver;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest {
+
+    @Test
+    public void startWebDriver(){
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.navigate().to("http://seleniumsimplified.com");
+
+        Assert.assertTrue("title should start differently",
+                            driver.getTitle().startsWith("Selenium Simplified"));
+
+        driver.close();
+    }
+}
