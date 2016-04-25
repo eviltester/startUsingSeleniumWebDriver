f9eef60293cbde0bcdb7f20bb04a1af78e9374d3
eviltester <3K1aDZQQgLtvBXuI>
2016-04-25 09:54:45 +0100

    updated for Marionette and Firefox 46

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ You can find the Free Online course here:
 
 The source code has only really been uploaded in case people encounter problems following the instructions or make a very simple mistake that they cannot figure out.
 
+## Update 25th April 2016
+
+Firefox 46 changes the way that WebDriver has to interact with it. So the built in `FirefoxDriver` will not work. You need to install Firefox 45 from [Firefox Extended Support Release](https://www.mozilla.org/en-US/firefox/organizations/all/) to continue using `FirefoxDriver`.
+
+If you want to use Firefox 46 and later then you need to use the [Marionette](https://developer.mozilla.org/en-US/docs/Mozilla/QA/Marionette/WebDriver) driver.
+
+Follow the instructions on the Marionette page to download it. You do not need to add it to your path, you can control that through a system property - see the code in this project for more detail.
+
+## Pre April 2016 updates
+
 The code has changed slightly because we now need to do a .close and a .quit to exit Firefox
 
 And I've updated the version of WebDriver.
--- src/test/java/com/eviltester/webdriver/MyFirstTest.java
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -3,14 +3,44 @@ package com.eviltester.webdriver;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
 
 public class MyFirstTest {
 
     @Test
     public void startWebDriver(){
 
-        WebDriver driver = new FirefoxDriver();
+        /**  NOTE: For Firefox 45 and below
+         * The following code will work on Firefox version 45 and below
+         * It will not work on Firefox 46 released in the week of 25th April 2016
+         * If you want to keep using version 45 then you can download it from here:
+         * https://www.mozilla.org/en-US/firefox/organizations/all/
+         */
+
+        //WebDriver driver = new FirefoxDriver();
+
+        /**   NOTE: If the test does not work
+         * Check your Firefox version. If you are on version 46 or above then
+         * you need to use the Marionette driver.
+         *
+         * Download the Marionette driver from here
+         * https://developer.mozilla.org/en-US/docs/Mozilla/QA/Marionette/WebDriver
+         *
+         * I assume you have unarchived the file into a /tools directory which would be a peer to
+         * the pom.xml file e.g.
+         *    pom.xml
+         *    /tools
+         *       /marionette
+         *           wires.exe
+         *
+         * The code below assumes a code structure like that. You will need to change
+         * the marionetteDriverLocation below if you choose a different location
+         */
+
+        String currentDir = System.getProperty("user.dir");
+        String marionetteDriverLocation = currentDir + "/tools/marionette/wires.exe";
+        System.setProperty("webdriver.gecko.driver", marionetteDriverLocation);
+        WebDriver driver = new MarionetteDriver();
 
         driver.navigate().to("http://seleniumsimplified.com");
 
