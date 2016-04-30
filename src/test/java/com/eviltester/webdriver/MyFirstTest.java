9c64813fa68f812b57c58fb4f1909ca79506578b
eviltester <3K1aDZQQgLtvBXuI>
2016-04-30 16:17:24 +0100

    updated to go back to FirefoxDriver until new version of Firefox is out which requires Marionette

--- src/test/java/com/eviltester/webdriver/MyFirstTest.java
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -3,7 +3,10 @@ package com.eviltester.webdriver;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.MarionetteDriver;
+// import the driver class to allow us to use Firefox Driver
+import org.openqa.selenium.firefox.FirefoxDriver;
+// uncomment the next line to use the MarionetteDriver
+//import org.openqa.selenium.firefox.MarionetteDriver;
 
 public class MyFirstTest {
 
@@ -12,35 +15,23 @@ public class MyFirstTest {
 
         /**  NOTE: For Firefox 45 and below
          * The following code will work on Firefox version 45 and below
-         * It will not work on Firefox 46 released in the week of 25th April 2016
+         * It will not work on Firefox versions which require Marionette (46 and above)
          * If you want to keep using version 45 then you can download it from here:
          * https://www.mozilla.org/en-US/firefox/organizations/all/
          */
 
-        //WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver();
 
-        /**   NOTE: If the test does not work
-         * Check your Firefox version. If you are on version 46 or above then
-         * you need to use the Marionette driver.
-         *
-         * Download the Marionette driver from here
-         * https://developer.mozilla.org/en-US/docs/Mozilla/QA/Marionette/WebDriver
-         *
-         * I assume you have unarchived the file into a /tools directory which would be a peer to
-         * the pom.xml file e.g.
-         *    pom.xml
-         *    /tools
-         *       /marionette
-         *           wires.exe
-         *
-         * The code below assumes a code structure like that. You will need to change
-         * the marionetteDriverLocation below if you choose a different location
+
+        /* The following code is for the Marionette Driver
+           You need to uncomment out the next 4 lines to use the marionette driver
+           You also need to download the Marionette executable
+           https://developer.mozilla.org/en-US/docs/Mozilla/QA/Marionette/WebDriver
          */
-
-        String currentDir = System.getProperty("user.dir");
-        String marionetteDriverLocation = currentDir + "/tools/marionette/wires.exe";
-        System.setProperty("webdriver.gecko.driver", marionetteDriverLocation);
-        WebDriver driver = new MarionetteDriver();
+        //String currentDir = System.getProperty("user.dir");
+        //String marionetteDriverLocation = currentDir + "/tools/marionette/wires.exe";
+        //System.setProperty("webdriver.gecko.driver", marionetteDriverLocation);
+        //WebDriver driver = new MarionetteDriver();
 
         driver.navigate().to("http://seleniumsimplified.com");
 
