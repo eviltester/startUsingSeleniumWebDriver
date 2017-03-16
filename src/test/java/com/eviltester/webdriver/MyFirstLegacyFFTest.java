fb26b71e5bea7d2a01e64469cf8027893cb0bb28
eviltester <3K1aDZQQgLtvBXuI>
2017-03-16 11:10:36 +0000

    updated because 3.3.1 needs system property set to run portable firefox and browser and tests need to run from the same drive in windows

--- src/test/java/com/eviltester/webdriver/MyFirstLegacyFFTest.java
+++ src/test/java/com/eviltester/webdriver/MyFirstLegacyFFTest.java
@@ -17,7 +17,11 @@ public class MyFirstLegacyFFTest {
            You do not neet to have Marionette geckodriver.exe or wires.exe on the path.
            You do need to be using v 3 of WebDriver */
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability("marionette", false);
+
+        // in firefox 3.2.0 and below you can choose the legacy driver with capabilities
+        //capabilities.setCapability("marionette", false);
+        // in firefox 3.3.1 and above you need to use system properties to use the legacy driver
+        System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"false");
 
         /* If Firefox < v48 is not your default browser then you need to tell WebDriver where it is,
            so uncomment the setCapability line below and amend the firefox_binary_path to the location of your firefox.exe
@@ -25,12 +29,21 @@ public class MyFirstLegacyFFTest {
                 https://sourceforge.net/projects/portableapps/files/Mozilla%20Firefox%2C%20Portable%20Ed./
            */
 
+
+        // *
+        // * YOU NEED TO SET THE PATH TO YOUR BINARY OF FIREFOX IF IT IS NOT IN THE PATH e.g. portable firefox
+        // *
+        // * Note: in v 3.3.1 the path needs to be on the same drive as you are running the tests from
+        // * e.g. you can't have the browsers on C:/ and the run the tests from D:/
+        // *
         // relative location to the project i.e. /tools/FirefoxPortable where /tools is at the same level as /src
         //String firefox_binary_path = new File(System.getProperty("user.dir"),"/tools/FirefoxPortable/FirefoxPortable.exe").getAbsolutePath();
         // hard coded location on C drive
-        String firefox_binary_path = "C:\\webdrivers\\FirefoxPortable\\FirefoxPortable.exe";
+        // String firefox_binary_path = "C:\\webdrivers\\FirefoxPortable\\FirefoxPortable.exe";
 
-        //capabilities.setCapability("firefox_binary", firefox_binary_path);
+        // * if you set a path above then uncomment this line to use it
+        // capabilities.setCapability("firefox_binary", firefox_binary_path);
+
         WebDriver driver = new FirefoxDriver(capabilities);
 
 
@@ -56,7 +69,7 @@ public class MyFirstLegacyFFTest {
      * https://sourceforge.net/projects/portableapps/files/Mozilla%20Firefox%2C%20Portable%20Ed./Mozilla%20Firefox%2C%20Portable%20Edition%2046.0.1/
      *
      * I extracted it to a tools directory at the same level of the source folder
-     * And then use the following code to start the firefox
+     * And then set the firefox_binary capability to point to the executable location
      */
 
 }
